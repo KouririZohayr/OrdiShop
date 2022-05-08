dd279d2df8e3199de88237564eb869508212c92b
Kouriri Zohayr <32511036+zoihair12@users.noreply.github.com>
2022-05-08 19:02:13 +0100

    new

--- js/script.js
+++ js/script.js
@@ -21,3 +21,7 @@ $(".owl-carousel").owlCarousel({
     }
 
 })
+var collapseElementList = [].slice.call(document.querySelectorAll('.collapse'))
+var collapseList = collapseElementList.map(function (collapseEl) {
+  return new bootstrap.Collapse(collapseEl)
+})
